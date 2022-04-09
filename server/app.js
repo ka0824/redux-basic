7989307f5e7186a4119d95ab664f843ddacf6a2f
ka0824 <rkgud0824@gmail.com>
2022-04-09 17:28:39 +0900

    feat: add server

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const { ok } = require('assert');
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const data = [
+    {
+        id: 1,
+        name: 'Kim',
+        score: 80
+    },
+    {
+        id: 2,
+        name: 'Lee',
+        score: 90
+    },
+    {
+        id: 3,
+        name: 'Choi',
+        score: 100
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send({message: ok, data: data})
+})
+
+app.listen(port, () => {
+    console.log('서버 오픈');
+})
